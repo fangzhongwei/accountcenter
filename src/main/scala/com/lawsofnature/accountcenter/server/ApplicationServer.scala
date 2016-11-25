a76d4dc3296cb11e5d000a3315ee0924dc0b8edc
fangzhongwei <zhongwei.fang@56qq.com>
2016-11-25 17:32:49 +0800

    first commit

--- src/main/scala/com/lawsofnature/accountcenter/server/ApplicationServer.scala
+++ src/main/scala/com/lawsofnature/accountcenter/server/ApplicationServer.scala
@@ -6,9 +6,10 @@ import Ice.ObjectImpl
 import com.google.inject.{AbstractModule, Guice}
 import com.google.inject.name.Names
 import com.lawsofnatrue.common.ice.{ConfigHelper, IceServerTemplate, IceServerTemplateImpl}
-import com.lawsofnature.accountcenter.service.AccountServiceImpl
+import com.lawsofnature.accountcenter.service.{AccountService, AccountServiceImpl}
 import com.lawsofnature.common.rabbitmq.{RabbitmqConsumerTemplate, RabbitmqConsumerTemplateImpl, RabbitmqProducerTemplate, RabbitmqProducerTemplateImpl}
 import com.lawsofnature.common.service.ConsumeService
+import com.typesafe.config.ConfigFactory
 import org.slf4j.LoggerFactory
 
 /**
@@ -23,12 +24,12 @@ object ApplicationServer extends App{
     override def configure() {
       val map: util.HashMap[String, String] = ConfigHelper.configMap
       Names.bindProperties(binder(), map)
-      bind(classOf[ConsumeService]).to(classOf[AccountServiceImpl]).asEagerSingleton()
+      bind(classOf[AccountService]).to(classOf[AccountServiceImpl]).asEagerSingleton()
       bind(classOf[RabbitmqConsumerTemplate]).to(classOf[RabbitmqConsumerTemplateImpl]).asEagerSingleton()
     }
   })
 
   private val consumerTemplate: RabbitmqConsumerTemplate = injector.getInstance(classOf[RabbitmqConsumerTemplate])
   consumerTemplate.connect
-  consumerTemplate.startConsume("", injector.getInstance(classOf[ConsumeService]))
+  consumerTemplate.startConsume(ConfigFactory.load().getString("account.mq.queue"), injector.getInstance(classOf[AccountService]))
 }
