6ec5d4f5fdcf98567ba9b3113c7d24e63b50a6e1
fangzhongwei <zhongwei.fang@56qq.com>
2016-12-07 19:13:40 +0800

    config

--- src/main/scala/com/lawsofnature/accountcenter/server/ApplicationServer.scala
+++ src/main/scala/com/lawsofnature/accountcenter/server/ApplicationServer.scala
@@ -2,21 +2,19 @@ package com.lawsofnature.accountcenter.server
 
 import java.util
 
-import Ice.ObjectImpl
-import com.google.inject.{AbstractModule, Guice}
 import com.google.inject.name.Names
-import com.lawsofnatrue.common.ice.{ConfigHelper, IceServerTemplate, IceServerTemplateImpl}
+import com.google.inject.{AbstractModule, Guice}
+import com.lawsofnatrue.common.ice.ConfigHelper
 import com.lawsofnature.account.repo.{AccountRepository, AccountRepositoryImpl}
 import com.lawsofnature.accountcenter.service.{AccountService, AccountServiceImpl}
-import com.lawsofnature.common.rabbitmq.{RabbitmqConsumerTemplate, RabbitmqConsumerTemplateImpl, RabbitmqProducerTemplate, RabbitmqProducerTemplateImpl}
-import com.lawsofnature.common.service.ConsumeService
+import com.lawsofnature.common.rabbitmq.{RabbitmqConsumerTemplate, RabbitmqConsumerTemplateImpl}
 import com.typesafe.config.ConfigFactory
 import org.slf4j.LoggerFactory
 
 /**
   * Created by fangzhongwei on 2016/11/21.
   */
-object ApplicationServer extends App{
+object ApplicationServer extends App {
   var logger = LoggerFactory.getLogger(this.getClass)
 
   private val injector = Guice.createInjector(new AbstractModule() {
