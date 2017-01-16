d59e2b1ec4cca3c4eb1682ac470a290241c08f4d
fangzhongwei <zhongwei.fang@56qq.com>
2017-01-16 11:17:13 +0800

    account center

--- accountclient/src/main/scala/com/lawsofnature/account/client/AccountClientService.scala
+++ accountclient/src/main/scala/com/lawsofnature/account/client/AccountClientService.scala
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory
 /**
   * Created by fangzhongwei on 2017/1/16.
   */
-trait AccountService {
+trait AccountClientService {
   def initClient
 
   def createAccount(traceId: String, memberId: Long): AccountBaseResponse
@@ -27,12 +27,12 @@ trait AccountService {
   def settle(traceId: String, request: SettleRequest): AccountBaseResponse
 }
 
-class AccountServiceImpl @Inject()(@Named("account.ice.client.init.config") iceInitConfig: String,
+class AccountClientServiceImpl @Inject()(@Named("account.ice.client.init.config") iceInitConfig: String,
                                    @Named("account.ice.client.init.size") iceInitSizeConfig: String,
                                    @Named("account.ice.client.init.size-max") iceInitSizeMaxConfig: String,
                                    @Named("account.ice.client.init.size-warn") iceInitSizeWarnConfig: String,
                                    @Named("account.ice.client.proxy.config") proxyConfig: String,
-                                   icePrxFactory: IcePrxFactory) extends AccountService {
+                                   icePrxFactory: IcePrxFactory) extends AccountClientService {
   val logger = LoggerFactory.getLogger(this.getClass)
 
   var accountEndpoint: AccountEndpointPrx = _
@@ -49,7 +49,7 @@ class AccountServiceImpl @Inject()(@Named("account.ice.client.init.config") iceI
 
   override def deposit(traceId: String, request: DepositRequest): DepositRequestResponse = accountEndpoint.deposit(traceId, request)
 
-  override def queryDepositOrder(traceId: String, paymentVoucherNo: String): Unit = accountEndpoint.queryDepositOrder(traceId, paymentVoucherNo)
+  override def queryDepositOrder(traceId: String, paymentVoucherNo: String): DepositResponse = accountEndpoint.queryDepositOrder(traceId, paymentVoucherNo)
 
   override def settle(traceId: String, request: SettleRequest): AccountBaseResponse = accountEndpoint.settle(traceId, request)
 }
